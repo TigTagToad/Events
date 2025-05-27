ef182130305568bb32288393ac644f961569b6b5
TigTagToad <taiga.arousse@gmail.com>
2025-05-27 19:26:42 +0100

    hid keys

--- events-website/src/utils/firebase.ts
+++ events-website/src/utils/firebase.ts
@@ -4,16 +4,14 @@ import { initializeAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAoNwVUz_qoppTcqwBL8XqkUJzdOF9JQtc",
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
   authDomain: "events-app-5be70.firebaseapp.com",
   projectId: "events-app-5be70",
   storageBucket: "events-app-5be70.firebasestorage.app",
-  messagingSenderId: "389607520199",
-  appId: "1:389607520199:web:658e4c87ec1aeee2124be2",
-  measurementId: "G-KMXL2GB3CH"
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDERKEY,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASEMEASID
 };
 
 // Initialize Firebase
